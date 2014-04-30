e8fbea50af6d21a11b56992e07e939c248d11aa7
ivettczimerman <cz.ivett@yahoo.ro>
2014-04-30 20:28:36 +0300

    Fix servlet

--- deutschtracker/src/main/java/de/dialogdata/aclabs/test/IServlet.java
+++ deutschtracker/src/main/java/de/dialogdata/aclabs/test/IServlet.java
@@ -2,6 +2,7 @@ package de.dialogdata.aclabs.test;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,12 +13,15 @@ public class IServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public void doGe(HttpServletRequest request, HttpServletResponse response)
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException{
+		
+		String nameP = request.getParameter("name");
+		
 				PrintWriter out = response.getWriter();
 				out.println("<html>");
 				out.println("<body>");
-				out.println("<h1>Name</h1>");
+				out.println("<h1>"+nameP+"</h1>");
 				out.println("</body>");
 				out.println("</html>");	
 			}
